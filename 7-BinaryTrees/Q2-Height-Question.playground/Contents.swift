15d39b0e118fbc8cbf67e7fc2e9aca066ccda503
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2025-11-29 21:33:42 +0300

    solve height of binary tree problem

--- 7-BinaryTrees/Q2-Height-Question.playground/Contents.swift
+++ 7-BinaryTrees/Q2-Height-Question.playground/Contents.swift
@@ -17,3 +17,69 @@ import XCTest
                   /    \
                 10      14
  */
+class Node {
+    var data: Int = 0
+    var left: Node?
+    var right: Node?
+    
+    init(_ key: Int) {
+        self.data = key
+    }
+    
+    func height() -> Int {
+        if hasChild(node: self) {
+            return 1 + max(left?.height() ?? 0, right?.height() ?? 0)
+        } else {
+            return 0
+        }
+    }
+    
+    func hasChild(node: Node) -> Bool {
+        return node.left != nil || node.right != nil
+    }
+}
+
+let root = Node(20)
+root.left = Node(8)
+root.right = Node(22)
+root.left?.left = Node(4)
+root.left?.right = Node(12)
+root.left?.right?.left = Node(10)
+root.left?.right?.right = Node(14)
+
+root.height()
+
+
+class Tests: XCTestCase {
+
+    func testHeightOfZero() {
+        let root = Node(5)
+        XCTAssertEqual(0, root.height())
+    }
+
+    func testHeightOfTwo() {
+        let root = Node(5)
+        root.left = Node(3)
+        root.right = Node(10)
+        root.left?.left = Node(20)
+        root.left?.right = Node(21)
+        root.right?.left = Node(1)
+
+        XCTAssertEqual(2, root.height())
+    }
+}
+
+// Infrastructure
+class TestObserver: NSObject, XCTestObservation {
+    func testCase(_ testCase: XCTestCase,
+                  didFailWithDescription description: String,
+                  inFile filePath: String?,
+                  atLine lineNumber: Int) {
+        assertionFailure(description, line: UInt(lineNumber))
+    }
+}
+let testObserver = TestObserver()
+XCTestObservationCenter.shared.addTestObserver(testObserver)
+Tests.defaultTestSuite.run()
+
+
