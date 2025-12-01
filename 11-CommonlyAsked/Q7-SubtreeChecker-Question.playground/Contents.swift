a71fde1d773f4f886678671cce18fbe58c462e04
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2025-12-01 17:43:02 +0300

    resolve sub tree checker problem

--- 11-CommonlyAsked/Q7-SubtreeChecker-Question.playground/Contents.swift
+++ 11-CommonlyAsked/Q7-SubtreeChecker-Question.playground/Contents.swift
@@ -31,7 +31,33 @@ class Node {
 }
 
 func isSubTree(_ tree: Node, _ subTree: Node) -> Bool {
-    return false
+    var orderedKeys = ""
+    var subOrderedKeys = ""
+    orderTreeKeys(tree, &orderedKeys)
+    orderTreeKeys(subTree, &subOrderedKeys)
+
+    return orderedKeys.contains(subOrderedKeys)
 }
 
-//isSubTree(root, subTree)
+func orderTreeKeys(_ node: Node?, _ keys: inout String) {
+    guard let node else {
+        return
+    }
+    keys.append(String(node.key))
+    orderTreeKeys(node.left, &keys)
+    orderTreeKeys(node.right, &keys)
+}
+
+let root = Node(5)
+root.left = Node(3)
+root.right = Node(7)
+root.left?.left = Node(2)
+root.left?.right = Node(4)
+root.right?.left = Node(6)
+root.right?.right = Node(8)
+
+let subTree = Node(7)
+subTree.left = Node(6)
+subTree.right = Node(8)
+
+isSubTree(root, subTree)
