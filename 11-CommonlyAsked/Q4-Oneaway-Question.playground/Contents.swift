41ce95d50ebacd92a3527d74d49e5d71c98b386a
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2025-12-01 15:32:37 +0300

    resolve one away problem

--- 11-CommonlyAsked/Q4-Oneaway-Question.playground/Contents.swift
+++ 11-CommonlyAsked/Q4-Oneaway-Question.playground/Contents.swift
@@ -17,7 +17,44 @@ import UIKit
  */
 
 func oneAway(_ first: String, _ second: String) -> Bool {
+    if abs(first.count - second.count) != 1, second.count != first.count {
+        return false
+    } else if first.count == second.count {
+        return replaceOneCharacter(first, second)
+    } else if first.count > second.count {
+        return removeOneCharacter(first, second)
+    } else {
+        return removeOneCharacter(second, first)
+    }
+}
+
+func replaceOneCharacter(_ first: String, _ second: String) -> Bool {
+    var firstArray = Array(first)
+    var secondArray = Array(second)
+    for i in 0..<first.count {
+        if firstArray[i] != secondArray[i] {
+            firstArray[i] = secondArray[i]
+            return firstArray == secondArray
+        }
+    }
+    
     return false
+}
+
+func removeOneCharacter(_ first: String, _ second: String) -> Bool {
+    var firstArray = Array(first)
+    var secondArray = Array(second)
+    for i in 0..<first.count {
+        if i == secondArray.count - 1 {
+            return firstArray.dropLast() == secondArray
+        }
+        
+        if firstArray[i] != secondArray[i] {
+            firstArray.remove(at: i)
+            return firstArray == secondArray
+        }
+    }
+    return firstArray == secondArray
 }
 
 oneAway("pale", "paleXXXX")     // false
