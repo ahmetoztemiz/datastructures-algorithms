1a9332b4b865f98f28d4e8e3801367a07afe0978
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2025-12-03 16:45:01 +0300

    resolve finding sum of given number google interview question

--- 13-Google/Q1-Google-Question.playground/Contents.swift
+++ 13-Google/Q1-Google-Question.playground/Contents.swift
@@ -12,8 +12,41 @@ import UIKit
  */
 
 func hasPairWithSum(_ arr: [Int], _ sum: Int) -> Bool {
+    let missingInt = arr.reduce(into: [Int: Int]()) { result, i in
+        result[i, default: 0] += 1
+    }
+    
+    for i in arr {
+        if let missingIntCount = missingInt[sum - i] {
+            if i == sum - i {
+                return missingIntCount > 1
+            } else {
+                return true
+            }
+        }
+    }
+    
+    return false
+}
+
+func hasPairWithSum2(_ arr: [Int], _ sum: Int) -> Bool {
+    var diff = Set<Int>()
+    for i in arr {
+        if diff.contains(sum - i) {
+            return true
+        }
+        
+        diff.insert(sum - i)
+    }
+    
     return false
 }
 
 hasPairWithSum([1, 2, 4, 9], 8)     // false
 hasPairWithSum([1, 2, 4, 4], 8)     // true
+hasPairWithSum([1, 2, 4], 8)     // false
+
+hasPairWithSum2([1, 2, 4, 9], 8)     // false
+hasPairWithSum2([1, 2, 4, 4], 8)     // true
+hasPairWithSum2([1, 2, 4], 8)     // false
+
