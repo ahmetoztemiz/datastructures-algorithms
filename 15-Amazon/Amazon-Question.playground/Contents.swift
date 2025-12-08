87d145da55b7d9889b93d6920d7a377031e489b2
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2025-12-08 14:08:20 +0300

    add test cases for merge arrays interview queiston

--- 15-Amazon/Amazon-Question.playground/Contents.swift
+++ 15-Amazon/Amazon-Question.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import XCTest
 
 /*
    _
@@ -36,5 +37,49 @@ func merge(_ A: [Int], _ B: [Int]) -> [Int] {
 }
 
 merge([1, 3, 5, 7], [2, 5, 8, 11, 14]) // [1, 2, 3, 5, 5, 7, 8, 11, 14]
+
+
+
+class Tests: XCTestCase {
+
+    func testMerge() {
+        let expected = [1, 2, 3, 5, 5, 7, 8, 11, 14]
+        let actual = merge([1, 3, 5, 7], [2, 5, 8, 11, 14])
+        XCTAssertEqual(expected, actual)
+    }
+ 
+    func testL1Empty() {
+        let expected = [2, 5, 8, 11, 14]
+        let actual = merge([], [2, 5, 8, 11, 14])
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testL2Empty() {
+        let expected = [1, 3, 5, 7]
+        let actual = merge([1, 3, 5, 7], [])
+        XCTAssertEqual(expected, actual)
+    }
+
+    func testNoOverlap() {
+        let expected = [1, 2, 3, 4, 5, 6]
+        let actual = merge([1, 2, 3], [4, 5, 6])
+        XCTAssertEqual(expected, actual)
+    }
+}
+
+// Infrastructure
+class TestObserver: NSObject, XCTestObservation {
+    func testCase(_ testCase: XCTestCase,
+                  didFailWithDescription description: String,
+                  inFile filePath: String?,
+                  atLine lineNumber: Int) {
+        assertionFailure(description, line: UInt(lineNumber))
+    }
+}
+let testObserver = TestObserver()
+XCTestObservationCenter.shared.addTestObserver(testObserver)
+Tests.defaultTestSuite.run()
+
+
 
 
