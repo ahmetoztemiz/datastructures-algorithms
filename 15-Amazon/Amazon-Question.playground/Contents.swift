d0dc11efd6f900ff68fbeef916f11e74d82ebcaf
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2025-12-08 14:03:03 +0300

    resolve merge arrays

--- 15-Amazon/Amazon-Question.playground/Contents.swift
+++ 15-Amazon/Amazon-Question.playground/Contents.swift
@@ -11,8 +11,28 @@ import UIKit
  */
 
 func merge(_ A: [Int], _ B: [Int]) -> [Int] {
-    // Do your work here...
-    return [Int]()
+    var i = 0; let m = A.count;
+    var j = 0; let n = B.count;
+    
+    var merged: [Int] = []
+    
+    while i < m || j < n {
+        if i == m {
+            merged.append(B[j])
+            j += 1
+        } else if j == n {
+            merged.append(A[i])
+            i += 1
+        } else if A[i] < B[j] {
+            merged.append(A[i])
+            i += 1
+        } else {
+            merged.append(B[j])
+            j += 1
+        }
+    }
+    
+    return merged
 }
 
 merge([1, 3, 5, 7], [2, 5, 8, 11, 14]) // [1, 2, 3, 5, 5, 7, 8, 11, 14]
