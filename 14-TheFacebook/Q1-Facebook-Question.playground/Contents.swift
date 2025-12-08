4a2db37966b8ca49669fc5668c175851d28ea2a6
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2025-12-08 13:31:43 +0300

    resove intersect array problem

--- 14-TheFacebook/Q1-Facebook-Question.playground/Contents.swift
+++ 14-TheFacebook/Q1-Facebook-Question.playground/Contents.swift
@@ -11,7 +11,24 @@ import UIKit
  */
 
 func intersect(_ A: [Int], _ B: [Int]) -> [Int] {
-    return [Int]()
+    var i = 0; let m = A.count
+    var j = 0; let n = B.count
+    
+    var intersectArray = [Int]()
+    
+    while i < m, j < n {
+        if A[i] < B[j] {
+            i += 1
+        } else if B[j] < A[i] {
+            j += 1
+        } else {
+            intersectArray.append(A[i])
+            i += 1
+            
+        }
+    }
+    
+    return intersectArray
 }
 
 intersect([1, 2, 4, 5, 6], [2, 3, 5, 7]) // [2, 5]
